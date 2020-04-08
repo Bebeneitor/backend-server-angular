650c929a7bdbd9e1ed3413481711252d24c32d91
Bebeneitor <50459522+Bebeneitor@users.noreply.github.com>
2020-04-08 17:18:09 -0500

    Removing key
    Removing Key

--- config/config.js
+++ config/config.js
@@ -3,4 +3,3 @@ module.exports.SEED = "este-es-un-seed-dificil";
 // Google
 module.exports.CLIENT_ID =
   "1087778403465-lgmddcgankq71t2j3o4f0l849piboocv.apps.googleusercontent.com";
-module.exports.SECRET_CLIENT = "eq4LuRQCH27lKIhstHcHkYXR";
